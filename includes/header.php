7c9962c75caff7d98d43da34993a6735a85aaf95
Dugsteer <dugsteer@gmail.com>
2021-12-08 13:28:10 +0100

    Commit

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Free, printable ESL vocabulary printable worksheets for learning English" />
+    <meta name="keywords" content="Text Maze Dominoes Games Worksheets ESL vocabulary" />
+    <title>Esl-ology Printables</title>
+    <link rel="icon" type="image/png" href="favicon.png" ; />
+    <link rel="stylesheet" href="./css/styles.css" />
+    <link rel="stylesheet" href="css/social.css">
+
+    <script data-ad-client="ca-pub-6192312197226967" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6192312197226967" crossorigin="anonymous"></script>
+</head>
+
+<body>
--- text-maze.php
+++ text-maze.php
@@ -1,39 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta
-      name="description"
-      content="Free, printable ESL vocabulary printable worksheets for learning English"
-    />
-    <meta
-      name="keywords"
-      content="Text Maze Dominoes Games Worksheets ESL vocabulary"
-    />
-    <title>Esl-ology Printables</title>
-    <link rel="icon" type="image/png" href="favicon.png" ; />
-    <link rel="stylesheet" href="./css/styles.css" />
-      <link rel="stylesheet" href="css/social.css">
 
-       <script data-ad-client="ca-pub-6192312197226967" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6192312197226967"
-     crossorigin="anonymous"></script>
-    <!-- <script src="csi.min.js"></script> -->
-  </head>
-  <body>
-
-    <?php include "./social.php" ?>
-    <?php include "./navbar.php" ?>
+    <?php include "./includes/header.php" ?>
+    <?php include "./includes/social.php" ?>
+    <?php include "./includes/navbar.php" ?>
 
     <div id="wrapper" class="wrapper">
     <div class="gridfiller"></div>
 
-    <?php include "./narrow-ad-top.php" ?>
-
-    <div class="sidebar-container" id="sidebar">
-        <?php include "./sidebar.html"; ?>
-    </div>
+    <?php include "./includes/narrow-ad-top.php" ?>
+    <?php include "./includes/sidebar.html"; ?>
 
     <?php 
             $imgsrc = "Minimaze.webp";
@@ -114,7 +88,7 @@
       ?>
 
 
-       <?php include "./narrow-ad-middle.php"?>
+       <?php include "./includes/narrow-ad-middle.php"?>
 
       <?php 
         $item_title = "Comparisons Text Maze";
@@ -183,9 +157,6 @@
     </div>
   </div>
      
-    <?php include "./footer.php" ?>
+    <?php include "./includes/footer.php" ?>
 
-    <script src="/script.js"></script>
-    
-  </body>
-</html>
+ 
