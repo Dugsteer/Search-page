9b3e72f4741c5627bcb270bb03e2ed60f06564f4
Dugsteer <dugsteer@gmail.com>
2021-12-09 11:03:32 +0100

    Xmas Go Fish

--- text-maze.php
+++ text-maze.php
@@ -1,191 +1,196 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta
-      name="description"
-      content="Free, printable ESL vocabulary printable worksheets for learning English"
-    />
-    <meta
-      name="keywords"
-      content="Text Maze Dominoes Games Worksheets ESL vocabulary"
-    />
-    <title>Esl-ology Printables</title>
-    <link rel="icon" type="image/png" href="favicon.png" ; />
-    <link rel="stylesheet" href="./css/styles.css" />
-      <link rel="stylesheet" href="css/social.css">
 
-       <script data-ad-client="ca-pub-6192312197226967" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6192312197226967"
-     crossorigin="anonymous"></script>
-    <!-- <script src="csi.min.js"></script> -->
-  </head>
-  <body>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Free, printable ESL vocabulary printable worksheets for learning English" />
+  <meta name="keywords" content="Text Maze Dominoes Games Worksheets ESL vocabulary" />
+  <title>Esl-ology Printables</title>
+  <link rel="icon" type="image/png" href="favicon.png" ; />
+  <link rel="stylesheet" href="./css/styles.css" />
+  <link rel="stylesheet" href="css/social.css">
 
-    <?php include "./social.php" ?>
-    <?php include "./navbar.php" ?>
+  <script data-ad-client="ca-pub-6192312197226967" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6192312197226967" crossorigin="anonymous"></script>
+  <!-- <script src="csi.min.js"></script> -->
+</head>
 
-    <div id="wrapper" class="wrapper">
+<body>
+
+  <?php include "./social.php" ?>
+  <?php include "./navbar.php" ?>
+
+  <div id="wrapper" class="wrapper">
     <div class="gridfiller"></div>
 
     <?php include "./narrow-ad-top.php" ?>
 
     <div class="sidebar-container" id="sidebar">
-        <?php include "./sidebar.html"; ?>
+      <?php include "./sidebar.html"; ?>
     </div>
 
-    <?php 
-            $imgsrc = "Minimaze.webp";
-            $title = "Text Mazes and Printables";
-            $text = '<p>Esl-ology Printables are worksheets that are free to print and
+    <?php
+    $imgsrc = "Minimaze.webp";
+    $title = "Text Mazes and Printables";
+    $text = '<p>Esl-ology Printables are worksheets that are free to print and
             download. There are
             <b style="font-weight: bold">vocabulary dominoes</b>,
             <b style="font-weight: bold">word searches</b>,
             <b style="font-weight: bold">text mazes</b> and more. Text mazes are
             a great alterative to traditional word searches and make great
             warmers or mini breaks.</p>';
-            include "./page-intro.php"; 
+    include "./page-intro.php";
     ?>
 
     <div class="main">
 
-      <?php 
-        $item_title = "Jobs Go Fish Cards";
-        $item_href = "JobsGoFish.pdf";
-        $item_img = "JobsGoFish.webp";
-        include "./page-item-tall.php"; 
+      <?php
+      $item_title = "Christmas Go Fish! Cards";
+      $item_href = "XmasGoFish.pdf";
+      $item_img = "XmasGoFish.webp";
+      include "./page-item-tall.php";
       ?>
 
-      <?php 
-        $item_title = "Fortnite Fever Text Maze";
-        $item_href = "FORTNITE FEVER.pdf";
-        $item_img = "FortniteFever.webp";
-        include "./page-item-tall.php"; 
+      <?php
+      $item_title = "Jobs Go Fish Cards";
+      $item_href = "JobsGoFish.pdf";
+      $item_img = "JobsGoFish.webp";
+      include "./page-item-tall.php";
       ?>
 
-     <?php 
-        $item_title = "Get a Job Game";
-        $item_href = "JobBoardGame.pdf";
-        $item_img = "JobsBoardGame.webp";
-        include "./page-item-tall.php"; 
+      <?php
+      $item_title = "Fortnite Fever Text Maze";
+      $item_href = "FORTNITE FEVER.pdf";
+      $item_img = "FortniteFever.webp";
+      include "./page-item-tall.php";
       ?>
 
-     <?php 
-        $item_title = "Sports Dominoes";
-        $item_href = "SportsDominoes.pdf";
-        $item_img = "SportsDominoes.webp";
-        include "./page-item-tall.php"; 
+      <?php
+      $item_title = "Get a Job Game";
+      $item_href = "JobBoardGame.pdf";
+      $item_img = "JobsBoardGame.webp";
+      include "./page-item-tall.php";
       ?>
 
-     <?php 
-        $item_title = "Foods Go Fish Cards";
-        $item_href = "FoodsGoFish.pdf";
-        $item_img = "FoodsGoFish.webp";
-        include "./page-item-tall.php"; 
+      <?php
+      $item_title = "Sports Dominoes";
+      $item_href = "SportsDominoes.pdf";
+      $item_img = "SportsDominoes.webp";
+      include "./page-item-tall.php";
       ?>
 
-     <?php 
-        $item_title = "Sports Go Fish Cards";
-        $item_href = "SportsGoFish.pdf";
-        $item_img = "SportsGoFish.webp";
-        include "./page-item-tall.php"; 
+      <?php
+      $item_title = "Foods Go Fish Cards";
+      $item_href = "FoodsGoFish.pdf";
+      $item_img = "FoodsGoFish.webp";
+      include "./page-item-tall.php";
       ?>
 
-      <?php 
-        $item_title = "Pre-Int Jeopardy Game";
-        $item_href = "PRE-INTERMILLIONAIRE.ppt";
-        $item_img = "preinterm.webp";
-        include "./page-item-short.php"; 
+      <?php
+      $item_title = "Sports Go Fish Cards";
+      $item_href = "SportsGoFish.pdf";
+      $item_img = "SportsGoFish.webp";
+      include "./page-item-tall.php";
       ?>
 
-      <?php 
-        $item_title = "Sports Board Game";
-        $item_href = "SportsBoardGame.pdf";
-        $item_img = "SportsBoardGame.webp";
-        include "./page-item-tall.php"; 
+      <?php
+      $item_title = "Pre-Int Jeopardy Game";
+      $item_href = "PRE-INTERMILLIONAIRE.ppt";
+      $item_img = "preinterm.webp";
+      include "./page-item-short.php";
       ?>
 
-      <?php 
-        $item_title = "Animals Go Fish! Cards";
-        $item_href = "AnimalsGoFish.pdf";
-        $item_img = "AnimalGoFish.webp";
-        include "./page-item-tall.php"; 
-      ?>
-
-
-       <?php include "./narrow-ad-middle.php"?>
-
-      <?php 
-        $item_title = "Comparisons Text Maze";
-        $item_href = "ComparativesSuperlativesTextMaze.pdf";
-        $item_img = "ComparativesSuperlativesTextMaze.webp";
-        include "./page-item-tall.php"; 
-      ?>
-      <?php 
-        $item_title = "Frequency Adverbs Text Maze";
-        $item_href = "FrequencyAdverbsTextMaze.pdf";
-        $item_img = "FrequencyAdverbsTextMaze.webp";
-        include "./page-item-tall.php"; 
-      ?>
-      <?php 
-        $item_title = "Past Simple Text Maze";
-        $item_href = "PastSimpleTextMaze.pdf";
-        $item_img = "PastSimpleTextMaze.webp";
-        include "./page-item-tall.php"; 
-      ?>
-      <?php 
-        $item_title = "Present Tenses  Text Maze";
-        $item_href = "PSimpleContinuous.pdf";
-        $item_img = "PSimpleContinuous.webp";
-        include "./page-item-tall.php"; 
-      ?>
-      <?php 
-        $item_title = "Clothes Text Maze";
-        $item_href = "ClothesTextMaze.pdf";
-        $item_img = "ClothesTextMaze.webp";
-        include "./page-item-tall.php"; 
-      ?>
-      <?php 
-        $item_title = "Vehicles Text Maze";
-        $item_href = "VehiclesTextMaze.pdf";
-        $item_img = "VehiclesTextMaze.webp";
-        include "./page-item-tall.php"; 
-      ?>
-      <?php 
-        $item_title = "Christmas Text Maze";
-        $item_href = "ChristmasTextMaze.pdf";
-        $item_img = "ChristmasTextMaze.webp";
-        include "./page-item-tall.php"; 
-      ?>
-      <?php 
-        $item_title = "Vehicles Word Search";
-        $item_href = "VehiclesWordSearch.pdf";
-        $item_img = "VehiclesWordSearch.webp";
-        include "./page-item-tall.php"; 
-      ?>
-      <?php 
-        $item_title = "Halloween Board Game";
-        $item_href = "HalloweenBoardGame.pdf";
-        $item_img = "HalloweenBoardGame.webp";
-        include "./page-item-tall.php"; 
+      <?php
+      $item_title = "Sports Board Game";
+      $item_href = "SportsBoardGame.pdf";
+      $item_img = "SportsBoardGame.webp";
+      include "./page-item-tall.php";
       ?>
 
       <?php include "./narrow-ad-middle.php" ?>
-      
-      <?php 
-        $item_title = "Halloween Dominoes";
-        $item_href = "HalloweenDominoesPrintable.pdf";
-        $item_img = "HalloweenDominoes.webp";
-        include "./page-item-tall.php"; 
+
+      <?php
+      $item_title = "Animals Go Fish! Cards";
+      $item_href = "AnimalsGoFish.pdf";
+      $item_img = "AnimalGoFish.webp";
+      include "./page-item-tall.php";
       ?>
+
+
+
+
+      <?php
+      $item_title = "Comparisons Text Maze";
+      $item_href = "ComparativesSuperlativesTextMaze.pdf";
+      $item_img = "ComparativesSuperlativesTextMaze.webp";
+      include "./page-item-tall.php";
+      ?>
+      <?php
+      $item_title = "Frequency Adverbs Text Maze";
+      $item_href = "FrequencyAdverbsTextMaze.pdf";
+      $item_img = "FrequencyAdverbsTextMaze.webp";
+      include "./page-item-tall.php";
+      ?>
+      <?php
+      $item_title = "Past Simple Text Maze";
+      $item_href = "PastSimpleTextMaze.pdf";
+      $item_img = "PastSimpleTextMaze.webp";
+      include "./page-item-tall.php";
+      ?>
+      <?php
+      $item_title = "Present Tenses  Text Maze";
+      $item_href = "PSimpleContinuous.pdf";
+      $item_img = "PSimpleContinuous.webp";
+      include "./page-item-tall.php";
+      ?>
+      <?php
+      $item_title = "Clothes Text Maze";
+      $item_href = "ClothesTextMaze.pdf";
+      $item_img = "ClothesTextMaze.webp";
+      include "./page-item-tall.php";
+      ?>
+      <?php
+      $item_title = "Vehicles Text Maze";
+      $item_href = "VehiclesTextMaze.pdf";
+      $item_img = "VehiclesTextMaze.webp";
+      include "./page-item-tall.php";
+      ?>
+      <?php
+      $item_title = "Christmas Text Maze";
+      $item_href = "ChristmasTextMaze.pdf";
+      $item_img = "ChristmasTextMaze.webp";
+      include "./page-item-tall.php";
+      ?>
+      <?php
+      $item_title = "Vehicles Word Search";
+      $item_href = "VehiclesWordSearch.pdf";
+      $item_img = "VehiclesWordSearch.webp";
+      include "./page-item-tall.php";
+      ?>
+      <?php
+      $item_title = "Halloween Board Game";
+      $item_href = "HalloweenBoardGame.pdf";
+      $item_img = "HalloweenBoardGame.webp";
+      include "./page-item-tall.php";
+      ?>
+
+      <?php
+      $item_title = "Halloween Dominoes";
+      $item_href = "HalloweenDominoesPrintable.pdf";
+      $item_img = "HalloweenDominoes.webp";
+      include "./page-item-tall.php";
+      ?>
+
+      <?php include "./narrow-ad-middle.php" ?>
 
     </div>
   </div>
-     
-    <?php include "./footer.php" ?>
 
-    <script src="/script.js"></script>
-    
-  </body>
+  <?php include "./footer.php" ?>
+
+  <script src="/script.js"></script>
+
+</body>
+
 </html>
